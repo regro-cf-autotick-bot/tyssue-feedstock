509e6cb7469def66e5478f5ff046bd86f10e4467
Guillaume Gay <guillaume@damcb.com>
2020-01-07 17:38:58 +0100

    trying to fix bug with VisualStudio

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,7 +6,7 @@ mkdir build && cd build
 set CMAKE_CONFIG="Release"
 set CMAKE_GENERATOR="NMake Makefiles"
 
-cmake -LAH                ^
+cmake -LAH                                    ^
   -DCMAKE_BUILD_TYPE="%CMAKE_CONFIG%"         ^
   -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%"      ^
   -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"   ^
