5c5b21b2190f1a17f56bf9d515d71b40b78ee79c
Guillaume Gay <guillaume@damcb.com>
2020-02-24 14:50:48 +0100

    v0.7.0-pre (#37)
     nmake insteas of cmake

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,4 @@
+setlocal EnableDelayedExpansion
 
 cd CGAL
 
@@ -6,14 +7,17 @@ mkdir build && cd build
 set CMAKE_CONFIG="Release"
 set CMAKE_GENERATOR="NMake Makefiles"
 
-cmake -LAH                                    ^
+cmake -LAH -G %CMAKE_GENERATOR%  -A x64       ^
   -DCMAKE_BUILD_TYPE="%CMAKE_CONFIG%"         ^
   -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%"      ^
   -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"   ^
   -DWITH_CGAL_ImageIO=OFF -DWITH_CGAL_Qt5=OFF ^
   ..
 if errorlevel 1 exit 1
-cmake --build . --config %CMAKE_CONFIG% --target INSTALL
+
+
+
+cmake --build . --config %CMAKE_CONFIG% --target INSTALL || goto :eof
 if errorlevel 1 exit 1
 
 cd ..\..
