208c0214ec2fc553dc55faf2b31a3bb28fb6ed56
Guillaume Gay <guillaume@damcb.com>
2020-12-07 09:55:50 +0100

    attempt at a correct bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,9 +5,13 @@ cd CGAL
 mkdir build && cd build
 
 set CMAKE_CONFIG="Release"
-set CMAKE_GENERATOR="NMake Makefiles JOM"
+set CMAKE_GENERATOR=Visual Studio 15 2017
+set CMAKE_GENERATOR_ARCH=x64
+set CMAKE_GENERATOR_TOOLSET=v141
 
 cmake -LAH -G %CMAKE_GENERATOR%               ^
+  -A "%CMAKE_GENERATOR_ARCH%"                 ^
+  -T "%CMAKE_GENERATOR_TOOLSET%"              ^
   -DCMAKE_BUILD_TYPE="%CMAKE_CONFIG%"         ^
   -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%"      ^
   -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"   ^
